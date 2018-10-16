2f4d87203f69b99b62a8fa32e12641a369c31939
EC2 Default User <ec2-user@ip-172-31-1-244.us-west-2.compute.internal>
2018-10-16 03:13:09 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get 'static_pages/home'
 
   get 'static_pages/help'
+  
+  get 'static_pages/about'
+  
+  root 'static_pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
