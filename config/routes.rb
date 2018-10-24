c923745ada608a48ba13ec1d79009184a2d9cee2
EC2 Default User <ec2-user@ip-172-31-43-200.us-west-2.compute.internal>
2018-10-24 18:09:13 +0000

    with bootstrap

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+  
+  get 'static_pages/about'
+  
+  root 'static_pages#home'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
