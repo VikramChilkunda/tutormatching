411dfd8300a118c6242c03c0fb26a7527e760b90
EC2 Default User <ec2-user@ip-172-31-7-169.us-east-2.compute.internal>
2018-11-06 16:39:31 +0000

    Got the signup and routes to work

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
+  get '/tuteesignup', to: 'tutees#new'
+
+  get '/home', to: 'static_pages#home'
+
+  get '/help', to: 'static_pages#help'
+  
+  get '/about', to: 'static_pages#about'
+  
   root 'static_pages#home'
-  get  '/help',    to: 'static_pages#help'
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
-  get  '/signup', to: 'tutees#signup'
-  get  '/tutorsignup', to: 'tutors#new'
+
+  post '/signup',  to: 'tutees#create'
+
+  resources :tutees
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
