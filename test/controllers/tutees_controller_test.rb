cfb7bbc1b144afa313e1885b359c5d8a58104c19
Adarsh Chilkunda <adroyalz@gmail.com>
2018-11-06 22:15:58 +0000

    Fix error in Tutees controller

--- test/controllers/tutees_controller_test.rb
+++ test/controllers/tutees_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class TuteesControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get tutee_signup_path
+    get tuteesignup_path
     assert_response :success
   end
 
