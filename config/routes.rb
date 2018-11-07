b1aad50b98b6544359067036c7b50a93910af2eb
Adarsh Chilkunda <adroyalz@gmail.com>
2018-11-07 22:59:25 +0000

    Make a very basic Tutor account page

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   post '/signup',  to: 'tutees#create'
 
   resources :tutees
+  
+  resources :tutors
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
