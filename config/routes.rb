64ff988fe1a67e229ea08c516bb4f1c93aef144d
EC2 Default User <ec2-user@ip-172-31-31-215.us-west-2.compute.internal>
2018-11-09 03:31:36 +0000

    modified signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   get  '/contact', to: 'static_pages#contact'
   get  '/signup', to: 'tutees#signup'
   get  '/tutorsignup', to: 'tutors#new'
-
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
