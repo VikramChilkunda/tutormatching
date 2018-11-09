de0b8acdde64921d4df2accf262374cd6c6c4ab4
EC2 Default User <ec2-user@ip-172-31-31-215.us-west-2.compute.internal>
2018-11-09 04:00:08 +0000

    added tutor signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,11 @@
 Rails.application.routes.draw do
-  get '/tuteesignup', to: 'tutees#new'
-  
-  get '/tutorsignup', to: 'tutors#new'
-
-  get '/home', to: 'static_pages#home'
-
-  get '/help', to: 'static_pages#help'
-  
-  get '/about', to: 'static_pages#about'
-  
   root 'static_pages#home'
-  
-  get  '/help',    to: 'static_pages#help'
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
+  get '/home',    to: 'static_pages#home'
+  get '/help',    to: 'static_pages#help'
+  get '/about',   to: 'static_pages#about'
   get  '/signup', to: 'tutees#signup'
-  get  '/tutorsignup', to: 'tutors#new'
- 
-
   post '/signup',  to: 'tutees#create'
+  get  '/tutorsignup', to: 'tutors#signup'
 
   resources :tutees
   
