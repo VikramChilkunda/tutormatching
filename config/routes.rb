df0c1017a7fdd9f7da6df19c151d073ad470a25e
Adarsh Chilkunda <adroyalz@gmail.com>
2018-11-30 03:52:46 +0000

    Fix @tutor and @tutee error by adding a second shared error messeages file

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/home',    to: 'static_pages#home'
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
-  get  '/signup', to: 'tutees#signup'
+  get  '/signup', to: 'tutees#new'
   post '/signup',  to: 'tutees#create'
   get  '/tutorsignup', to: 'tutors#new'
   post '/tutorsignup', to: 'tutors#create'
