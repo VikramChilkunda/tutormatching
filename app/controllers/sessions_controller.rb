527891e4bf41053b0d3a6a2bf8aefdf1292271e3
Adarsh Chilkunda <adroyalz@gmail.com>
2019-01-09 22:17:08 +0000

    Make integration tests for the login sessions, create a session helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  def create 
+    tutor = Tutor.find_by(email: params[:session][:email].downcase)
+    if tutor && tutor.authenticate(params[:session][:password])
+      log_in tutor
+      redirect_to tutor
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+  
+  def destroy
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,15 @@
 module SessionsHelper
+ def log_in(tutor)
+    session[:tutor_id] = tutor.id
+ end
+ 
+ def current_tutor
+   if session[:tutor_id]
+    @current_tutor ||= Tutor.find_by(id: session[:tutor_id])
+   end
+ end
+ 
+ def logged_in?
+  !current_tutor.nil?
+ end
 end
