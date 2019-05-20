11de344a3d590fa71deb28d58b7dba5d586951f6
Adarsh Chilkunda <adroyalz@gmail.com>
2019-05-20 22:03:22 +0000

    Made subject creation actually work

--- app/controllers/subject_controller.rb
+++ app/controllers/subject_controller.rb
@@ -4,15 +4,21 @@ class SubjectController < ApplicationController
   end
   
   def create 
-    @subject = Subject.new(:name => params[:name], :date => params[:date], :rate => params[:rate])
+    @subject = Subject.new(:name => subject_params[:name], :date => subject_params[:date], :rate => subject_params[:rate])
     if @subject.save
       flash[:success] = "Created Subject"
-      # redirect_to action: "show", id: @tutor.id
+       redirect_to subjectcreate_path
     else
       #flash[:error] = @subject.errors.full_messages
      # redirect_back(fallback_location: '/home')
+     redirect_to subjectcreate_path
     end
   end
+  
+  def showSubject
+    return @subject
+  end
+  
   private
     def subject_params
       params.require(:subject).permit(:name, :date, :rate)
