d571a5e3100cfc902510acd010989212db93493f
Adarsh Chilkunda <adroyalz@gmail.com>
2019-05-21 21:57:18 +0000

    Fixed a problem with error messages on subject creation (render worked, error messages do not survive redirect_to)

--- app/controllers/subject_controller.rb
+++ app/controllers/subject_controller.rb
@@ -7,10 +7,11 @@ class SubjectController < ApplicationController
     @subject = Subject.new(:name => subject_params[:name], :date => subject_params[:date], :rate => subject_params[:rate])
     if @subject.save
       flash[:success] = "Created Subject"
-      # redirect_to action: "show", id: @tutor.id
+      render 'new'
     else
       #flash[:error] = @subject.errors.full_messages
      # redirect_back(fallback_location: '/home')
+     render 'new'
     end
   end
   private
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,12 +1,12 @@
 class Subject < ApplicationRecord
     
     include ActiveModel::Model
-    attr_accessor :name, :date, :rate
-    def self.from_tutor_id(id)
-        tutor = Tutor.find(id)
-        person = Person.find(tutor.people_id)
-        new(name: person.name, email: person.email, grade: tutor.grade, id_num: tutor.id_num)
-    end
+     attr_accessor :name, :date, :rate
+    # def self.from_tutor_id(id)
+    #     tutor = Tutor.find(id)
+    #     person = Person.find(tutor.people_id)
+    #     new(name: person.name, email: person.email, grade: tutor.grade, id_num: tutor.id_num)
+    # end
     
     
     #belongs_to :tutor, foreign_key: :subject_id, inverse_of: :subject
