b97713d1e15936986b8650006ab670d398a55153
EC2 Default User <jack.mawhinney@yahoo.com>
2019-05-24 15:02:23 +0000

    Login Stuff

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -53,4 +53,34 @@ module SessionsHelper
   def store_location
     session[:forwarding_url] = request.original_url if request.get?
   end
+  
+  def remember(person)
+    person.remember
+    cookies.permanent.signed[:user_id] = person.id
+    cookies.permanent[:remember_token] = person.remember_token
+  end
+  
+   def current_person
+    if (person_id = session[:person_id])
+      @current_person ||= Person.find_by(id: person_id)
+    elsif (person_id = cookies.signed[:person_id])
+      person = Person.find_by(id: person_id)
+      if person && person.authenticated?(cookies[:remember_token])
+        log_in person
+        @current_person = person
+      end
+    end
+   end
+   
+   def forget(person)
+    person.forget
+    cookies.delete(:person_id)
+    cookies.delete(:remember_token)
+   end
+  
+   def log_out
+    forget(current_person)
+    session.delete(:person_id)
+    @current_person = nil
+   end
 end
