5df0482bcb95909537b62e9c971f9b410d67dd8b
Adarsh Chilkunda <adroyalz@gmail.com>
2019-08-08 19:42:36 +0000

    More heroku

--- db/migrate/20190808054100_remove_rate_from_subjects.rb
+++
@@ -1,4 +0,0 @@
-class RemoveRateFromSubjects < ActiveRecord::Migration[5.0]
-  def change
-  end
-end
--- db/migrate/20190808060350_remove_paid_from_subjects.rb
+++
@@ -1,5 +0,0 @@
-class RemovePaidFromSubjects < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :subjects, :paid, :boolean
-  end
-end
