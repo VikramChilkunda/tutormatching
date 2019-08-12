187d00670c0440510c305f436dee1d80de0893db
Adroyalz2 <thismine@gmail.com>
2019-08-12 18:20:57 +0000

    heroku stuff?

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190810060422) do
+ActiveRecord::Schema.define(version: 20190812010225) do
 
   create_table "people", force: :cascade do |t|
     t.datetime "created_at",                      null: false
