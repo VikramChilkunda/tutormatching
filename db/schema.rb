fcd23293883a4754e1dccd895a567137624699fe
EC2 Default User <jack.mawhinney@yahoo.com>
2019-08-20 19:18:35 +0000

    Implemented the drop down menu for subjects that works

--- db/schema.rb
+++ db/schema.rb
@@ -43,9 +43,9 @@ ActiveRecord::Schema.define(version: 20190814235321) do
   create_table "subjects", force: :cascade do |t|
     t.string   "name"
     t.string   "date"
+    t.string   "rate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "rate"
     t.integer  "creatorid"
   end
 
