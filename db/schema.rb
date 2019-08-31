bfd084f96624365172d21a4e1bcc41139b661f3a
TEDxCVHS <alexgiantseed@gmail.com>
2019-08-31 03:21:03 +0000

    adarsh here you go

--- db/schema.rb
+++ db/schema.rb
@@ -43,9 +43,9 @@ ActiveRecord::Schema.define(version: 20190830164425) do
   create_table "subjects", force: :cascade do |t|
     t.string   "name"
     t.string   "date"
-    t.string   "rate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "rate"
     t.integer  "creatorid"
   end
 
