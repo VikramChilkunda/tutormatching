0a910edab32ceefefd747e166e3cf77d1658c8e7
TEDxCVHS <alexgiantseed@gmail.com>
2019-10-19 06:57:35 +0000

    dsaf

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,8 +1,9 @@
-$(document).ready(function(){
+$(document).on('turbolinks:load', function() {
     $(".fa-bars").on("click", function(){
         $(".side").toggleClass("sidepopup");
     });
     $(".fa-times").on("click", function(){
         $(".side").removeClass("sidepopup");
-    }); 
+    });
 });
+ 
