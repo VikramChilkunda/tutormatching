cd7c13443eaa8d1c13cedaf74046354fa670d8b9
adroyalz <adroyalz@gmail.com>
2020-08-17 06:34:56 +0000

    deleted the temporary migrations

--- db/migrate/20200817060354_remove_remember_digest_from_people.rb
+++
@@ -1,5 +0,0 @@
-class RemoveRememberDigestFromPeople < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :people, :remember_digest, :string
-  end
-end
--- db/migrate/20200817060447_drop_subjects.rb
+++
@@ -1,5 +0,0 @@
-class DropSubjects < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :subjects
-  end
-end
