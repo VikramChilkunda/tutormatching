0299b9c8667d658ca37a3b1c675d54e607e3981a
kevindong8 <kevindongofficial@gmail.com>
2022-01-13 09:05:39 -0800

    update emails

--- test/models/tutor_test.rb
+++ test/models/tutor_test.rb
@@ -40,17 +40,26 @@ class TutorTest < ActiveSupport::TestCase
     assert_not @tutor.valid?
   end
   
-   test "email validation should accept valid addresses" do
-    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
-                         first.last@foo.jp alice+bob@baz.cn]
+  #  test "email validation should accept valid addresses" do
+  #   valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+  #                        first.last@foo.jp alice+bob@baz.cn]
+  #   valid_addresses.each do |valid_address|
+  #     @tutor.email = valid_address
+  #     assert @tutor.valid?, "#{valid_address.inspect} should be valid"
+  #   end
+  # end
+  
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@gusd.net user@stu.gusd.net USER@gusd.NET USER@stu.gusd.NET]
     valid_addresses.each do |valid_address|
       @tutor.email = valid_address
       assert @tutor.valid?, "#{valid_address.inspect} should be valid"
     end
   end
-  
+
   test "email validation should reject invalid addresses" do
-    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+    invalid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                            first.last@foo.jp alice+bob@baz.cn user@example,com user_at_foo.org user.name@example.
                            foo@bar_baz.com foo@bar+baz.com foo@bar..com]
     invalid_addresses.each do |invalid_address|
       @tutor.email = invalid_address
