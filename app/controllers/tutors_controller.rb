c80991ebca0e0c836ba4f61eb047a43c8f2d3b15
kevindong8 <kevindongofficial@gmail.com>
2022-01-18 10:12:03 -0800

    activation wip

--- app/controllers/tutors_controller.rb
+++ app/controllers/tutors_controller.rb
@@ -55,6 +55,7 @@ class TutorsController < ApplicationController
             @tutor.update_attribute(:adminOverride, nil)
             flash[:success] = "Tutor signup successful!"
             redirect_to controller: "people", action: "show", id: (@tutor.people_id)
+            #
           else 
             if @person.errors.any? || @tutor.errors.any?
               render 'new'
---
+++ db/migrate/20220118181013_add_activation_to_user.rb
@@ -0,0 +1,4 @@
+class AddActivationToUser < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
