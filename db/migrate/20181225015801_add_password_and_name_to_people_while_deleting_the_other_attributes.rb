89211b3e1d6709fdf4d582826c2f158e9652e6ae
kevindong8 <kevindongofficial@gmail.com>
2022-01-27 08:44:11 -0800

    edits

--- db/migrate/20181225015801_add_password_and_name_to_people_while_deleting_the_other_attributes.rb
+++ db/migrate/20181225015801_add_password_and_name_to_people_while_deleting_the_other_attributes.rb
@@ -1,7 +1,7 @@
 class AddPasswordAndNameToPeopleWhileDeletingTheOtherAttributes < ActiveRecord::Migration[5.0]
   def change
     change_table :people do |t|
-      t.remove :integer, :tutee_id, :tutor_id
+      t.remove :integer, :tutor_id
       t.string :name, before: :created_at
       t.string :password_digest, before: :created_at
     end
